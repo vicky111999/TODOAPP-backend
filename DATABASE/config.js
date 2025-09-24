bd9300dc564bb9084eb7767e6cce800e284808dc
vicky111999 <vigneshsaravanan261@gmail.com>
2025-09-24 20:29:25 +0530

    deploy

--- DATABASE/config.js
+++ DATABASE/config.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose"
 
 const connectDB =()=>{
 
-    mongoose.connect(process.env.DB_URI)
+
+    mongoose.connect(process.env.DB_URI,{
+         useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
         .then(()=>{console.log("DataBase Connected")})
         .catch((error)=>{console.log("ERROR",error)})
 }
