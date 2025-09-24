080812ec0161e7e16f9951973ccfcffe84d5525e
vicky111999 <vigneshsaravanan261@gmail.com>
2025-09-24 12:06:13 +0530

    last

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
 app.use(express.json())
-app.use(cors({origin:"http://localhost:5173"}))
+app.use(cors())
 
 app.use("/api/user",routes)
 
