64683a47885cfc40a19d9dbe9866e7b43db240d2
vicky111999 <vigneshsaravanan261@gmail.com>
2025-10-29 10:56:48 +0530

    updated

--- Routes/todolistroute.js
+++ Routes/todolistroute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { itemAdd, itemDelete } from "../controller/todoitemcontroller.js"
+import { itemAdd } from "../controller/todoitemcontroller.js"
 import { categories, Pending } from "../controller/todoitemviewscontroller.js"
 import { todoallitemviewcontroller } from "../controller/todoallitemviewcontroller.js"
 import { alltask } from "../controller/todoalltaskController.js"
@@ -10,7 +10,6 @@ const routes = express.Router()
 
 routes.post("/todoAdd",itemAdd)
 routes.get("/Categories",categories)
-routes.post("/itemDelete",itemDelete)
 routes.get("/",todoallitemviewcontroller)
 routes.get("/pending",Pending)
 routes.get("/alltask",alltask)
