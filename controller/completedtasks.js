9fda21d1959648cf15326e5ccf651188e5c15b4e
vicky111999 <vigneshsaravanan261@gmail.com>
2025-10-30 12:59:11 +0530

    update

--- controller/completedtasks.js
+++ controller/completedtasks.js
@@ -5,10 +5,10 @@ import todomodel from "../DATABASE/todomodel.js"
 export const completedtask = async(req,res)=>{
 
     try{
-    const find = todomodel.find({completed:true})
+    const find = await todomodel.find({completed:true})
     if(!find) return res.status(404).json({success:false,message:"NO tasks available"})
 
-        return res.json(200).json({success:true,data:find})
+        return res.status(200).json({success:true,data:find})
 }
 catch(error){
     return res.status(500).json({success:false,message:error.message})
