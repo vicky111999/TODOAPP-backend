e3bbb577739b58486f8e9041f5e5edd07f2628b2
vicky111999 <vigneshsaravanan261@gmail.com>
2025-10-31 11:37:18 +0530

    pending

--- DATABASE/config.js
+++ DATABASE/config.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose"
 const connectDB =()=>{
 
 
-    mongoose.connect(process.env.DB_URI,{
-         useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    mongoose.connect(process.env.DB_URI)
         .then(()=>{console.log("DataBase Connected")})
         .catch((error)=>{console.log("ERROR",error)})
 }
